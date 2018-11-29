ee5a99f7cb21669e2e5c5ec3a14bb7285c4dfb09
AMITESH SHARMA <amitesh.sharma@gmail.com>
2018-11-29 10:58:48 +0530

    first comit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Home } from './screens/home'
+export default class App  {
+  render() {
+    return (
+      <View>
+        <Home />
+      </View>
+    );
+  }
+}
+
